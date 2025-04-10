ea5eb72a8adc784a600f553d65b4cdaec1ccaef2
diya-kailash <diyakailash04@gmail.com>
2025-04-10 20:45:40 +0530

    Update navbar branding from "VIT Placement Portal" to "placeVIT" in login and register pages

--- student/login.php
+++ student/login.php
@@ -65,7 +65,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container">
-            <a class="navbar-brand" href="../index.php">VIT Placement Portal</a>
+            <a class="navbar-brand" href="../index.php">placeVIT</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
